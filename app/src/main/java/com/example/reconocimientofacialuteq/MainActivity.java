6df17fbb3dc7177d8fcfe2bb27e82b9e58c24a94
Xaavier <xavier.munoz.pisco@gmail.com>
2020-08-25 15:50:26 -0500

    Activar Camara

--- app/src/main/java/com/example/reconocimientofacialuteq/MainActivity.java
+++ app/src/main/java/com/example/reconocimientofacialuteq/MainActivity.java
@@ -1,14 +1,151 @@
 package com.example.reconocimientofacialuteq;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int RQS_OPEN_DOCUMENT_TREE = 2;
+    ImageView imageView;
+    TextView textInfo;
+    private static Bitmap imageBitmap;
+
+    Socket miSockte;
+    TextView respuesta;
+    private Socket socket;
+
+    private static final int SERVERPORT = 5555;
+    private static final String SERVER_IP = "192.168.1.15";
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    private static final int PICK_IMAGE = 100;
+    Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayList<String> permisos = new ArrayList<String>();
+        permisos.add(Manifest.permission.CAMERA);
+        permisos.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        permisos.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        permisos.add(Manifest.permission.WRITE_CALENDAR);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        textInfo = (TextView) findViewById(R.id.info);
+        getPermission(permisos);
+    }
+    public void getPermission(ArrayList<String> permisosSolicitados) {
+
+        ArrayList<String> listPermisosNOAprob = getPermisosNoAprobados(permisosSolicitados);
+        if (listPermisosNOAprob.size() > 0)
+            if (Build.VERSION.SDK_INT >= 23)
+                requestPermissions(listPermisosNOAprob.toArray(new String[listPermisosNOAprob.size()]), 1);
+
+    }
+    public ArrayList<String> getPermisosNoAprobados(ArrayList<String> listaPermisos) {
+        ArrayList<String> list = new ArrayList<String>();
+        for (String permiso : listaPermisos) {
+            if (Build.VERSION.SDK_INT >= 23)
+                if (checkSelfPermission(permiso) != PackageManager.PERMISSION_GRANTED)
+                    list.add(permiso);
+        }
+        return list;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        String s = "";
+        if (requestCode == 1) {
+            for (int i = 0; i < permissions.length; i++) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED)
+                    s = s + "OK " + permissions[i] + "\n";
+                else
+                    s = s + "NO  " + permissions[i] + "\n";
+            }
+            Toast.makeText(this.getApplicationContext(), s, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void CargarFotoGaleria(View view){
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode){
+                case REQUEST_IMAGE_CAPTURE:
+                    Bundle extras = data.getExtras();
+                    imageBitmap = (Bitmap) extras.get("data");
+                    new Thread(new ClientThread()).start();
+                    imageView.setImageBitmap(imageBitmap);
+                    break;
+                case PICK_IMAGE:
+                    imageUri = data.getData();
+                    try {
+                        imageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),imageUri);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    imageView.setImageURI(imageUri);
+                    new Thread(new ClientThread()).start();
+                    break;
+            }
+        }
+    }
+    class ClientThread implements Runnable {
+
+        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+        @Override
+        public void run() {
+            try {
+                socket = new Socket(SERVER_IP, SERVERPORT);
+                DataOutputStream salida;
+
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                byte[] byteArray = stream.toByteArray();
+                String mens="nombre";
+                TextView salidaTextView = (TextView) findViewById(R.id.textView);
+                try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream())) {
+                    objectOutputStream.writeObject(byteArray);
+                    objectOutputStream.writeObject(mens);
+                }
+            } catch (UnknownHostException e1) {
+                e1.printStackTrace();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+
+        }
+
     }
 }
