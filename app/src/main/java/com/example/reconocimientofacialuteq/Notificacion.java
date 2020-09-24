c5a2edd2412de3aeef7ddafa109de8efad829b9f
Xaavier <xavier.munoz.pisco@gmail.com>
2020-09-23 20:22:33 -0500

    Guardar img en firebase y notificar

--- app/src/main/java/com/example/reconocimientofacialuteq/Notificacion.java
+++ app/src/main/java/com/example/reconocimientofacialuteq/Notificacion.java
@@ -2,7 +2,9 @@ package com.example.reconocimientofacialuteq;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class Notificacion extends AppCompatActivity {
@@ -11,6 +13,10 @@ public class Notificacion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notificacion);
+        Bundle bundle = getIntent().getExtras();
+        String uri= bundle.getString("img");
+        Log.d("TAG", "Uri : " + uri);
+        Log.d("TAG", "Uri 2 : " + Uri.parse(uri));
        /* TextView textView =findViewById(R.id.textviNOT);
         String message= getIntent().getStringExtra("message");
         textView.setText(message);*/
